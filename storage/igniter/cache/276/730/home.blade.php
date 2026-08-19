39e3035238df59433b8d94c37407fc4023c004f8
Amine EL ALAOUI MRANI <mranidev@proton.me>
2026-08-19 10:26:22 -0400

    Fix public/storage symlink for Wasmer package builder

---
+++ storage/igniter/cache/276/730/home.blade.php
@@ -0,0 +1,6 @@
+<?php 
+/* /home/lone/TastyIgniter/TastySetup/vendor/tastyigniter/ti-theme-orange/resources/views/_pages/home.blade.php */
+class Pagic6a85b9dd5e379260972171_5b80164507ad41be4bad280f074cf89eClass extends Igniter\Main\Template\Code\PageCode
+{
+
+}
---
+++ storage/igniter/cache/c3d/902/default.blade.php
@@ -0,0 +1,6 @@
+<?php 
+/* /home/lone/TastyIgniter/TastySetup/vendor/tastyigniter/ti-theme-orange/resources/views/_layouts/default.blade.php */
+class Pagic6a85b9dd5d6dd692651562_7c31af995cdbc8a38d1ce9e78583c956Class extends Igniter\Main\Template\Code\LayoutCode
+{
+
+}
